338c1b5f2ef833cd07c535f3b6cbad28d2e1836c
Takuya Yonezawa <takuya.yonezawa@japan-d2.com>
2024-07-25 11:20:12 +0900

    chore: Update BedrockChat model to latest version
    This commit updates the BedrockChat model to the latest version (`anthropic.claude-3-haiku-20240307-v1:0`). It also removes unnecessary console logging and adds a stream for processing chat responses.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
-
 import { BedrockChat } from "@langchain/community/chat_models/bedrock";
 
-export const main = async (): Promise<void> => {
-  const model = new BedrockChat({
-    region: 'us-east-1',
-    model: 'anthropic.claude-v2:1',
-    maxTokens: 2048,
-    temperature: 0,
-    cache: false,
-    verbose: true,
-  });
-
-  const res = await model.invoke([['human', 'LangChainとは何ですか？']]);
-  console.log(JSON.stringify(res, null, 2));
+console.log = (msg) => {
+	process.stdout.write(`${msg}`);
 };
 
-main()
+export const main = async (): Promise<void> => {
+	const model = new BedrockChat({
+		region: "us-east-1",
+		model: "anthropic.claude-3-haiku-20240307-v1:0",
+		maxTokens: 2048,
+		temperature: 0,
+		cache: false,
+		verbose: false,
+	});
+
+	const stream = await model.stream([["human", "LangChainとは何ですか？"]]);
+	for await (const chunk of stream) {
+		console.log(chunk.content);
+		// process.stdout.write(chunk.content);
+	}
+
+	// console.log(res.)
+	// console.log(JSON.stringify(res, null, 2));
+};
+
+main();
